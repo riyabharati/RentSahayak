cff833f7d6b45c29916e404795e37ada607a44e8
hrichamaharjan <richoo234@gmail.com>
2022-05-18 16:48:05 +0545

    Navbar  added

---
+++ R-s-Frontend/myapp/src/components/Navbar/NavbarItem.js
@@ -0,0 +1,35 @@
+export const commonNavItems=[
+    {
+        title:"About",
+        url:'#',
+        cName:'navs-text'
+    },
+    {
+        title:"Categories",
+        url:'#',
+        cName:'navs-text'
+    },
+    {
+        title:"Our Team",
+        url:'#',
+        cName:'navs-text'
+    },
+    {
+        title:"Contact Us",
+        url:'#',
+        cName:'navs-text'
+    },
+]
+
+export const notLoggedInNavItems = [
+    {
+        title:"Login",
+        url:'/login',
+        cName:'navs-text'
+    },
+    {
+        title:"Register",
+        url:'/register',
+        cName:'navs-text'
+    }
+]
