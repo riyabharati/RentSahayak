47a58f48e9354ba1fc3ac1691ebac90e5a264487
Anjalipantha1 <panthaanjali12>
2022-05-31 21:42:26 +0545

    authorizer.ts is added

---
+++ RS-Admin-Backend/src/middleware/authorizer.ts
@@ -0,0 +1,24 @@
+import { FieldTypeUserJWT, UserCategory } from '../models/user/user.types'
+import { UserModel } from '../models/user/userModel'
+import { commonResponse, responseCode, userResponse } from '../utils/constants'
+
+const authorizerAdmin = (req, res, next) => {
+  const { userId } = req.loggedInUser as FieldTypeUserJWT
+
+  UserModel.findById(userId)
+    .then((user) => {
+      if (!user) {
+        next({ message: userResponse.error.USER_NOT_FOUND, status: responseCode.NOT_FOUND })
+      } else if (user.category !== UserCategory.ADMIN) {
+        next({ message: commonResponse.error.UNAUTHORIZED_ACCESS, status: responseCode.FORBIDDEN })
+      } else {
+        next()
+      }
+    })
+    .catch(() => {
+      next({})
+    })
+}
+
+
+export { authorizerAdmin, authorizerCompany, authorizerCustomer }
