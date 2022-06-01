cb6ee7d2e1aca19c0179a42bf4bf0bbe31cb77c1
Anjalipantha1 <panthaanjali12>
2022-06-01 14:49:29 +0545

    authorizer.ts is updated

--- RS-Admin-Backend/src/middleware/authorizer.ts
+++ RS-Admin-Backend/src/middleware/authorizer.ts
@@ -19,6 +19,40 @@ const authorizerAdmin = (req, res, next) => {
       next({})
     })
 }
+const authorizerCompany = (req, res, next) => {
+  const { userId } = req.loggedInUser as FieldTypeUserJWT
 
+  UserModel.findById(userId)
+    .then((user) => {
+      if (!user) {
+        next({ message: userResponse.error.USER_NOT_FOUND, status: responseCode.NOT_FOUND })
+      } else if (user.category !== UserCategory.RENTER) {
+        next({ message: commonResponse.error.UNAUTHORIZED_ACCESS, status: responseCode.FORBIDDEN })
+      } else {
+        next()
+      }
+    })
+    .catch(() => {
+      next({})
+    })
+}
+
+const authorizerCustomer = (req, res, next) => {
+  const { userId } = req.loggedInUser as FieldTypeUserJWT
+
+  UserModel.findById(userId)
+    .then((user) => {
+      if (!user) {
+        next({ message: userResponse.error.USER_NOT_FOUND, status: responseCode.NOT_FOUND })
+      } else if (user.category !== UserCategory.TENANT) {
+        next({ message: commonResponse.error.UNAUTHORIZED_ACCESS, status: responseCode.FORBIDDEN })
+      } else {
+        next()
+      }
+    })
+    .catch(() => {
+      next({})
+    })
+}
 
 export { authorizerAdmin, authorizerCompany, authorizerCustomer }
