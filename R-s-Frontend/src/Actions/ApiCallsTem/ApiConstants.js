278e0f7e9883d540486b13ba082da6134dd88368
Siddhanta Lamichhane <siddhantlmc123@gmail.com>
2022-06-15 15:11:01 +0545

    commit

---
+++ R-s-Frontend/src/Actions/ApiCallsTem/ApiConstants.js
@@ -0,0 +1,22 @@
+//AUTHENTICATION
+
+
+const auth = {
+    getUser: '/user/me',
+    register: '/user/register',
+    login: '/user/login',
+    forgotPassword: '/user/forgot-password',
+    resetPassword: '/user/reset-password',
+    changePassword: '/user/change-password',
+    verifyUser: '/user/verify',
+    resendOtp: '/user/resendOtp'
+  }
+  
+  
+  
+  
+  export const apiEndPoints = {
+    auth,
+    
+  }
+  
