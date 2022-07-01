01a2108dd78c148bd6408012b2f356a32bab2971
hrichamaharjan <richoo234@gmail.com>
2022-07-01 18:35:17 +0545

    property page  modified

---
+++ R-s-Frontend/myapp/src/components/Property/Most_viewed/data.js
@@ -0,0 +1,48 @@
+export const Data={
+
+    cardData:[
+        {
+            id:1,
+            img:"assets/images/hello.png",
+            title:"card1",
+            desc:"desc1"
+
+        },
+        {
+            id:1,
+            img:"assets/images/sweethome.png",
+            title:"card2",
+            desc:"des2"
+
+        },
+        {
+            id:1,
+            img:"assets/images/building.png",
+            title:"card3",
+            desc:"des3"
+
+        },
+        {
+            id:1,
+            img:"assets/images/hello.png",
+            title:"card4",
+            desc:"des4"
+
+        },
+        {
+            id:1,
+            img:"assets/images/hello.png",
+            title:"card5",
+            desc:"des5"
+
+        },
+        {
+            id:1,
+            img:"assets/images/hello.png",
+            title:"card6",
+            desc:"des6"
+
+        },
+       
+    ]
+}
