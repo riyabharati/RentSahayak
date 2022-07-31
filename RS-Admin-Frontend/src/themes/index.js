6c3a6e1fb725d4a141534f5c5dc47a44031dc016
Anjalipantha1 <panthaanjali12>
2022-07-31 13:15:36 +0545

    RS-Admin-frontend is updated

---
+++ RS-Admin-Frontend/src/themes/index.js
@@ -0,0 +1,30 @@
+import defaultTheme from "./default";
+
+import { createTheme } from "@material-ui/core";
+
+const overrides = {
+  typography: {
+    h1: {
+      fontSize: "3rem",
+    },
+    h2: {
+      fontSize: "2rem",
+    },
+    h3: {
+      fontSize: "1.64rem",
+    },
+    h4: {
+      fontSize: "1.5rem",
+    },
+    h5: {
+      fontSize: "1.285rem",
+    },
+    h6: {
+      fontSize: "1.142rem",
+    },
+  },
+};
+
+export default {
+  default: createTheme({ ...defaultTheme, ...overrides }),
+};
